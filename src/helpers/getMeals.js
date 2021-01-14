c7662d9b780ec5d7410f42d1f433c50e43f57960
Joseph Burke <joseph_burke@outlook.com>
2021-01-14 13:28:40 +0000

    Set num of results to 100. Add vegan health label

--- src/helpers/getMeals.js
+++ src/helpers/getMeals.js
@@ -1,7 +1,7 @@
 export default async function() {
-  const { REACT_APP_ID: id, REACT_APP_KEY: key } = process.env
+  const { REACT_APP_ID: id, REACT_APP_KEY: key } = process.env;
   const data = await fetch(
-    `https://api.edamam.com/search?q=vegan&to=50&app_id=${id}&app_key=${key}`
+    `https://api.edamam.com/search?q=vegan&health=vegan&to=100&app_id=${id}&app_key=${key}`
   ).then(response => response.json());
   return data.hits;
 };

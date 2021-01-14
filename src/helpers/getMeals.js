bf67cab606fa02b32832e3b73c100e6e2ce68016
Joseph Burke <joseph_burke@outlook.com>
2021-01-14 11:40:28 +0000

    Implement getMeals data-fetch function

---
+++ src/helpers/getMeals.js
@@ -0,0 +1,7 @@
+export default async function() {
+  const { REACT_APP_ID: id, REACT_APP_KEY: key } = process.env
+  const data = await fetch(
+    `https://api.edamam.com/search?q=vegan&app_id=${id}&app_key=${key}`
+  ).then(response => response.json());
+  return data.hits;
+};

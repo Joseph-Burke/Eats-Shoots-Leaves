201bebe2c4cb1523de7780c84efba959a0a116f2
Joseph Burke <joseph_burke@outlook.com>
2021-01-18 01:31:25 +0000

    Improve content and styles of Recipe component

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,59 +1,79 @@
 import { connect } from "react-redux";
 import { useParams } from "react-router-dom";
+import Header from "../components/Header";
+import styles from "./Recipe.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClock } from "@fortawesome/free-solid-svg-icons";
 
 const Recipe = ({ meals }) => {
   let { id } = useParams();
   const meal = meals.find(meal => meal.key === parseInt(id));
 
-  console.log(meal);
-
   const ingredients = (
     <div>
-      <h3>Ingredients</h3>
-      <ul>
-        {meal.ingredientLines.map(line => (
-          <li>{line}</li>
-        ))}
-      </ul>
+      <dt>Ingredients</dt>
+      {meal.ingredientLines.map(line => (
+        <dd>{line}</dd>
+      ))}
     </div>
   );
 
   const healthLabels = (
-    <div>
-      <h3>Health Labels</h3>
-      <ul>
-        {meal.healthLabels.map(label => (
-          <li>{label}</li>
-        ))}
-      </ul>
-    </div>
+    <>
+      {meal.healthLabels.map(label => (
+        <span className="badge bg-success bg-gradient mr-1">{label}</span>
+      ))}
+    </>
   );
 
   const cautions = (
-    <div>
-      <h3>Cautions</h3>
-      <ul>
-        {meal.cautions.map(label => (
-          <li>{label}</li>
-        ))}
-      </ul>
-    </div>
+    <>
+      {meal.cautions.map(label => (
+        <span className="badge bg-danger bg-gradient mr-1">{label}</span>
+      ))}
+    </>
   );
 
-  const timeStatement = meal.totalTime == 0 ? "Time Unknown" : `Time: ${meal.totalTime} minutes`;
-  const totalTime = <p>{timeStatement}</p>;
+  const time =
+    meal.totalTime == 0 ? null : (
+      <span>
+        <FontAwesomeIcon
+          icon={faClock}
+          transform="grow-5"
+          className="mr-2 ml-1"
+        />
+        {meal.totalTime} minutes
+      </span>
+    );
 
   return (
-    <div>
-      <h1>{meal.label}</h1>
-      <h3>by "{meal.source}"</h3>
-      <img src={meal.image}></img>
-      {totalTime}
-      {ingredients}
-      {healthLabels}
-      {cautions}
-      <p>Calories: {Math.floor(meal.calories)}</p>
-    </div>
+    <>
+      <Header homeURL="../"></Header>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-6">
+            <img className="card-img-top" src={meal.image} />
+          </div>
+          <div className="card-body col-6 py-0">
+            <h1 className="card-title">{meal.label}</h1>
+            <h3 className="card-subtitle text-muted font-weight-lighter">
+              from {meal.source}
+            </h3>
+            <div className={styles["font-size-20"]}>
+              {healthLabels}
+              {cautions}
+            </div>
+            <div
+              className={`d-flex justify-content-between mt-2 ${styles["font-size-20"]}`}
+            >
+              {time}
+              <span>Calories: {Math.floor(meal.calories)}</span>
+            </div>
+            {ingredients}
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
